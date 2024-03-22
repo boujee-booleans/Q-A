b415bdf1a8095d80ab47c88969199a8c21ab253f
MwaniN <brooklynewyork@gmail.com>
2024-03-22 14:31:24 -0400

    Loading progress

--- load_csv_data.js
+++ load_csv_data.js
@@ -5,20 +5,44 @@ const client = require('./db.js')
 
 // inputfile & target table
 var inputFile = path.join(__dirname, '/csv_data/answers_photos.csv')
-var targetTable = 'public.answers_photos'
+var targetTable = 'answers_photos'
 
 
-var stream = client.query(copyFrom(`COPY ${targetTable} FROM CSV HEADER STDIN`))
+var stream = client.query(copyFrom(`COPY ${targetTable} FROM STDIN WITH (FORMAT csv, HEADER true)`))
 var fileStream = fs.createReadStream(inputFile)
 
-fileStream.on('error', (error) =>{
-  console.log(`Error in reading file: ${error}`)
-})
-stream.on('error', (error) => {
-  console.log(`Error in copy command: ${error}`)
-})
-stream.on('end', () => {
-  console.log(`Completed loading data into ${targetTable}`)
-  client.end()
-})
-fileStream.pipe(stream);
+// COPY ${targetTable} FROM STDIN WITH (FORMAT csv, HEADER true)
+
+const executeQuery = (targetTable) => {
+  const execute = (target, callback) => {
+      client.query(`Truncate ${target}`, (err) => {
+              if (err) {
+              client.end()
+              callback(err)
+              // return console.log(err.stack)
+              } else {
+              console.log(`Truncated ${target}`)
+              callback(null, target)
+              }
+          })
+  }
+  execute(targetTable, (err) =>{
+      if (err) return console.log(`Error in Truncate Table: ${err}`)
+      var stream = client.query(copyFrom(`COPY ${targetTable} FROM STDIN`))
+      var fileStream = fs.createReadStream(inputFile)
+
+      fileStream.on('error', (error) =>{
+          console.log(`Error in creating read stream ${error}`)
+      })
+      stream.on('error', (error) => {
+          console.log(`Error in creating stream ${error}`)
+      })
+      stream.on('end', () => {
+          console.log(`Completed loading data into ${targetTable}`)
+          client.end()
+      })
+      fileStream.pipe(stream);
+  })
+}
+// Execute the function
+executeQuery(targetTable)
