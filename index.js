64a9b935c4727a136b85a99b93f7769d9a33277d
MwaniN <brooklynewyork@gmail.com>
2024-03-23 18:55:27 -0400

    index.js

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const path = require("path");
 const app = express();
 app.use(express.json())
 
+/// GET qa/questions
+// parameters -product.id, page, count
+// does not include any reported questions
+
+// GET /qa/questions/:question_id/answers
+// does not include any reported answers
+
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
